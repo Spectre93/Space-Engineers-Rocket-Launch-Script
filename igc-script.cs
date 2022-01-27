8b5111a9d0ccbb7ca33a689b99a53986e23c9da5
Daniel <djaniboe@gmail.com>
2022-01-27 16:10:27 +0100

    Rename script to better reflect its function

--- igc-script.cs
+++ igc-script.cs
@@ -41,3 +41,13 @@ private void TriggerTimerFromString(string str){
     }catch(Exception e){
         Echo("'[" + str + "]' timer block missing.");
     }
+}
+
+
+/*
+Todo
+test sending tuples
+
+var t = (speed: 4.5, altitude: 3);
+Echo($"Speed is {t.speed} and rocket is currently at {t.altitude} meters up);
+*/
